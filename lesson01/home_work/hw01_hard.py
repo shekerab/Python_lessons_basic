9258091b10b90be2cadab5345b7180df099c3ea6
legiss <shbo@legis-s.ru>
2018-05-13 10:30:58 +0300

    initial commit

--- lesson01/home_work/hw01_hard.py
+++ lesson01/home_work/hw01_hard.py
@@ -1,5 +1,4 @@
-
-__author__ = 'Ваши Ф.И.О.'
+__author__ = 'Шекера Борис'
 
 # Задание-1:
 # Ваня набрал несколько операций в интерпретаторе и получал результаты:
@@ -14,3 +13,22 @@ __author__ = 'Ваши Ф.И.О.'
 # если точно известно, что её значение не изменялось?
 
 # Подсказка: это значение точно есть ;)
+
+
+class f:
+    def __init__(self,num):
+        self.num = num
+    def __eq__(self, other):
+        return True
+    def __gt__(self, other):
+        return True
+    def __mul__(self, other):
+        return self.num*other
+    def __pow__(self, other):
+        return self.num**other
+
+a = f(1)
+print(a == a**2, a == a*2, a > 999999)
+
+a = float('inf')
+print(a == a**2, a == a*2, a > 999999)
